0aa5c4c294d1126247a5ab62347221f7806889ea
daichaodeyu <47140420+daichaodeyu@users.noreply.github.com>
2020-04-03 22:23:51 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 App({
   globalData: {
     // 连接的域名：注意格式，不要带端口号
-    server_domain: "wss://www.chenzhongsheng.cn/mqtt",
+    server_domain: "wss://www.域名/mqtt",
     //本工程的订阅的主题
-    subTopic: "/test/onoff/",
+    subTopic: "/onoff/",
     //本工程的f发送的主题
-    pubTopic: "/test/onoff/"
+    pubTopic: "/onoff/"
   }
 })
